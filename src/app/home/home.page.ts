7eab7934ce14e1949580bb4620a582baa7c8c0a0
Apollostowel <apollostowel@icloud.com>
2023-06-20 16:35:11 +0300

    Updating site

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,18 +8,25 @@ import { Component } from '@angular/core';
 export class HomePage {
   projects = [
     {
+      title: 'Fullstack Library Project',
+      url: '../library',
+      subtitle: 'React, Redux, Typescript, C#',
+      img: './assets/frontend.png',
+      description: 'A full-service library website with its own database, user authentication and authorization, with many admin features.'
+    },
+    {
       title: 'E-commerce Website Project',
       url: '../frontend',
       subtitle: 'React, Redux, Typescript',
       img: './assets/frontend.png',
-      description: 'A mockup full ecommerce website, featuring products and their categories, a shopping cart, and user login with admin functionality.'
+      description: 'A mockup full ecommerce website, featuring products, categories, a cart, and user login with admin functionality.'
     },
     {
       title: 'OMDB Project',
       url: '../omdb',
       subtitle: 'JavaScript',
       img: './assets/omdb.png',
-      description: 'The OMDB Movie Search app allows the user to search the OMDB for movie or tv shows, the page then displaying the results. The results are shown in groups of not greater than ten for each page.'
+      description: 'The OMDB Movie Search app allows the user to search the OMDB for movie or tv shows, the page displaying the results.'
     },
     {
       title: 'Budget App Project',
@@ -33,14 +40,14 @@ export class HomePage {
       url: '../tmdb-app',
       subtitle: 'React Native, Node',
       img: './assets/tmdbRN.png',
-      description: 'Similar to project in Ionic Angular, connects to the The Movie Database API, but with its own backend server, and more functionality.'
+      description: 'Connects to the The Movie Database API, but with its own backend server for user authentication and authorization.'
     },
     {
       title: 'TMDB Project',
       url: '../movies',
       subtitle: 'Ionic Angular',
       img: './assets/tmdb.png',
-      description: 'Simple app that connects to the The Movie Database API.'
+      description: 'Simple app that connects to the The Movie Database API, using Ionic Angular.'
     },
     {
       title: 'Memegenerator Project',
@@ -54,14 +61,14 @@ export class HomePage {
       url: '../restcountries',
       subtitle: 'JavaScript',
       img: './assets/restcountries.png',
-      description: 'Using the same bootstrapped template of the OMDB project, RestCountries searches country info and returns the results for each country.'
+      description: 'Using the same template of the OMDB project, RestCountries searches country info and returns the results.'
     },
     {
       title: 'Todo List',
       url: '../todo',
       subtitle: 'JavaScript',
       img: './assets/todo.png',
-      description: 'My first Javascript project was a simple to-do list. It should remember tasks between visits, with the ability to edit and undo.'
+      description: 'A simple to-do list. It should remember tasks between visits, with the ability to edit and undo.'
     },
     {
       title: 'Storefront Project',
@@ -71,7 +78,7 @@ export class HomePage {
       description: 'A default template Storefront project, exploring CSS design and animations.'
     }
 
-  ]
+  ];
 selectedIndex: number;
 i: number;
 
